7e4e8030bacf0aa5e3fb577b1182576055861938
Nevin Leh <nevinleh@gmail.com>
2021-04-26 22:39:04 -0600

    some logging stuff

---
+++ CT/CT_tools.py
@@ -0,0 +1,5 @@
+import logging
+
+log = logging.getLogger(__name__)
+def do():
+    log.info("Hello there")
---
+++ CT/main.py
@@ -0,0 +1,7 @@
+import logging
+import sys
+import CT_tools as ct
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
